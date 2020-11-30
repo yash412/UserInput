6a42df1d8533cabf9d88532c2baeb43a20b64b24
Yash Choudhari <yashchoudhari.ysc@gmail.com>
2020-11-30 21:10:26 +0530

    Added proper sync with order button

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -21,6 +21,8 @@ import java.text.NumberFormat;
  */
 public class MainActivity extends AppCompatActivity {
 
+    int quantity = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,8 +33,23 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(2);
-        displayPrice(2*5);
+        displayPrice (quantity * 5);
+    }
+
+    /**
+     * This method is called when the plus button is clicked.
+     */
+    public void increment (View view) {
+        quantity = quantity + 1;
+        display(quantity);
+    }
+
+    /**
+     * This method is called when the minus button is clicked.
+     */
+    public void decrement (View view) {
+        quantity = quantity - 1;
+        display(quantity);
     }
 
     /**
