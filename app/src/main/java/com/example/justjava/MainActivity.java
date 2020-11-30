e2fd2a5d498aded5f8d42c5fe82dd97dba49f71b
Yash Choudhari <yashchoudhari.ysc@gmail.com>
2020-11-30 11:51:37 +0530

    Added price tag

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -31,8 +31,8 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(1);
-        displayPrice(1*5);
+        display(2);
+        displayPrice(2*5);
     }
 
     /**
